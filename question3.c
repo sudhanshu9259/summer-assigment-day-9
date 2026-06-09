0cfe5bb75b0d1313c8ec3f7e62fa5c64b3175f6b
Sudhanshu Yadav <sudhanshuy991@gamil.com>
2026-06-09 22:04:45 +0530

    third question

---
+++ question3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    char ch;
+
+    for(i = 1; i <= 5; i++) {
+        ch = 'A' + i - 1;
+
+        for(j = 1; j <= i; j++) {
+            printf("%c", ch);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
